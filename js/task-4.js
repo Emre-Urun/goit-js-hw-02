c479e3e7f4a8dd796f837acaaffa70426146f4a4
Emre-Urun <emrern268@gmail.com>
2025-06-01 02:36:12 +0300

    update 1.0.1

--- js/task-4.js
+++ js/task-4.js
@@ -7,26 +7,21 @@ let jamaica = 120;
 function getShippingCost(country) {
   switch (country) {
     case "Australia":
-      console.log(`Shipping to ${country} will cost ${australia} credits`);
-      break;
+      return `Shipping to ${country} will cost ${australia} credits`;
     case "China":
-      console.log(`Shipping to ${country} will cost ${china} credits`);
-      break;
+      return `Shipping to ${country} will cost ${china} credits`;
     case "Chile":
-      console.log(`Shipping to ${country} will cost ${chile} credits`);
-      break;
+      return `Shipping to ${country} will cost ${chile} credits`;
     case "Jamaica":
-      console.log(`Shipping to ${country} will cost ${jamaica} credits`);
-      break;
+      return `Shipping to ${country} will cost ${jamaica} credits`;
     default:
-      console.log("Sorry, there is no delivery to your country");
+      return "Sorry, there is no delivery to your country";
   }
-  return;
 }
 
-getShippingCost("Australia"); // "Shipping to Australia will cost 170 credits"
-getShippingCost("Germany"); // "Sorry, there is no delivery to your country"
-getShippingCost("China"); // "Shipping to China will cost 100 credits"
-getShippingCost("Chile"); // "Shipping to Chile will cost 250 credits"
-getShippingCost("Jamaica"); // "Shipping to Jamaica will cost 120 credits"
-getShippingCost("Sweden"); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
